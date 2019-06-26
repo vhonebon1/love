6b22d903c602d2f96fd04a38f7e91bfa6ec4d5c2
verity <verity.honebon@royalacademy.org.uk>
2019-06-26 17:16:00 +0100

    add colours

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 import Picker from './components/picker';
 import People from './people';
 
-const colours = ["#3cd070", "#e0115f", "#ff9933", "#ccccff", "#002444"]
-const totalCount = People.length;
-
 class App extends React.Component {
 
   constructor(props) {
@@ -19,26 +16,20 @@ class App extends React.Component {
 
   handleMatch = () => {
     const { firstPick, secondPick } = this.state;
-    setTimeout(this.randomFirstPick(), 4000);
-    setTimeout(this.randomSecondPick(), 7000);
+    setTimeout(this.randomPick(), 4000);
+    setTimeout(this.randomPick(false), 7000);
   }
 
   clearMatch = () => {
     this.setState({ firstPick: false, secondPick: false })
   }
 
-  randomFirstPick = () => {
-    const randomIndex = Math.floor((Math.random() * totalCount) + 1);
-    this.setState({ firstPick: People[randomIndex] })
-  }
-
-  randomSecondPick = () => {
-    const randomIndex = Math.floor((Math.random() * totalCount) + 1);
-    this.setState({ secondPick: People[randomIndex] })
+  randomPick = (first = true) => {
+    const randomIndex = Math.floor((Math.random() * People.length) + 1);
+    first ? this.setState({ firstPick: People[randomIndex] }) : this.setState({ secondPick: People[randomIndex] })
   }
 
   render() {
-    console.log(People)
     const { firstPick, secondPick } = this.state;
     return (
       <div className="App">
--- src/components/headBlock.jsx
+++ src/components/headBlock.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Spinner from './spinner';
 
+const colours = ["#3cd070", "#e0115f", "#ff9933", "#ccccff", "#002444"];
+
 const HeadBlock = ({ person }) =>
   <React.Fragment>
-    <div>{person}</div>
+    <div style={{backgroundColor: colours[Math.floor((Math.random() * colours.length) + 1)]}}>{person}</div>
     <Spinner />
   </React.Fragment>
 
