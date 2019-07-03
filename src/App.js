f97298f20efa40cf0fc8a70efcd2a3d4ce11eb96
Verity <vhonebon@gmail.com>
2019-07-03 23:30:00 +0100

    save axios and call via https

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
 
   getWeather = () => {
     const key = 'd75d6d8d03bc31b75bc9f8783bc53aa8';
-    const endpoint = `http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&?units=metric&APPID=${key}`
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&?units=metric&APPID=${key}`
     axios.get(endpoint).then((response) => this.setWeather(response))
   }
 
