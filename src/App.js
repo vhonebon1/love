0738fcf24b319700f3066ff5fea61ec991217d9b
verity <verity.honebon@royalacademy.org.uk>
2019-07-08 16:54:06 +0100

    Add countdown

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { show: false }
+    this.state = { show: false, daysUntil: this.daysUntilStart() }
   }
 
   toggleChoice = show => this.setState({ show });
 
   clearChoice = () => this.setState({ show: false });
+
+  daysUntilStart = () => {
+    const today = new Date();
+    const dt1 = new Date(`${today.getMonth() + 1}/${(today.getDate())}/${today.getFullYear()}`)
+    const dt2 = new Date("08/16/2019");
+    const diffTime = Math.abs(dt2.getTime() - dt1.getTime());
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  }
 
   renderChosenComponent = () => {
     const { show } = this.state;
@@ -34,10 +42,10 @@ class App extends React.Component {
   }
 
   render() {
-    const { show } = this.state;
+    const { show, daysUntil } = this.state;
     return (
       <div className="App">
-        <Header clearChoice={this.clearChoice} />
+        <Header clearChoice={this.clearChoice} daysUntil={daysUntil}/>
         { !show && <Weather /> }
         <Nav toggleChoice={this.toggleChoice} mini={show} />
         { show &&
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Chart from '../images/chart.svg';
 
-const Header = ({ clearChoice }) =>
+const Header = ({ clearChoice, daysUntil }) =>
   <div className="header">
     <div className="dashboard__header" onClick={() => clearChoice()}>
-      <div>
-        <img src={Chart} alt=""/>
-        <h4>Conference 2019</h4>
+      <div className="dashboard__headerInner">
+        <div>
+          <img src={Chart} alt=""/>
+          <h4>Conference 2019</h4>
+        </div>
+        <div className="countdown">
+          <div className="countdown__inner">
+            <h1>{daysUntil}</h1>
+            <div>days</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
